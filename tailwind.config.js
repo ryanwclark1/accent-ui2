3c4e7fb89f867d60ed3a228e37a77e8717552dcf
Ryan Clark <ryanc@accentservices.com>
2024-04-03 12:42:01 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+const formsPlugin = require('@tailwindcss/forms')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['**/*.{html,templ}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/typography')],
+}
