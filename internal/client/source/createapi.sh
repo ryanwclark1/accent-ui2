14ca2a5108ff4024ea2f8567496ee37ca83860d9
Ryan Clark <ryanc@accentservices.com>
2024-04-05 14:24:52 -0500

    createserver

--- internal/client/source/createapi.sh
+++ internal/client/source/createapi.sh
@@ -16,26 +16,26 @@ for file in $files; do
     filename_no_ext_hyphens="${filename_no_accent//-/}"
 
     # Generate code using openapi-generator-cli
-    # https://openapi-generator.tech/docs/generators/go
+    # https://openapi-generator.tech/docs/generators/go-server
     docker run -e --rm \
     -e GO_POST_PROCESS_FILE='/workdir/go-post-process-file.sh' \
     -v ${PWD}:/workdir openapi-generator-cli generate \
     --enable-post-process-file \
     -i /workdir/${filename} \
-    -g go \
-    --additional-properties=packageName=${filename_no_ext_hyphens},packageVersion=0.0.1,prependFormOrBodyParameters=true,enumClassPrefix=true,isGoSubmodule=true,structPrefix=true,generateMarshalJSON=true,generateInterfaces=true,useOneOfDiscriminatorLookup=false,withAWSV4SignaturewithAWSV4Signature=false,withGoMod=false,withXml=false,hideGenerationTimestamp=true \
+    -g go-server \
+    --additional-properties=packageName=${filename_no_ext_hyphens}server,packageVersion=0.0.1,addResponseHeader=true,enumClassPrefix=falsem,featureCORS,hideGenerationTimestamp=false,onlyInterfaces=true,outputAsLibrary=true,router=chi,serverport=8081,sourceFolder=src \
     --git-user-id ryanwclark \
     --git-repo-id accent-voice \
     --global-property=generateAliasAsModel \
-    -o /workdir/out/client/${filename_no_accent}
+    -o /workdir/out/server/${filename_no_accent}
 
 
     # Change ownership of newly created files and directories recursively
     docker run \
-    -v ${PWD}:/workdir alpine chown -R $(id -u):$(id -g) /workdir/out/client/${filename_no_accent}
+    -v ${PWD}:/workdir alpine chown -R $(id -u):$(id -g) /workdir/out/server/${filename_no_accent}
 
     docker run \
-    -v ${PWD}:/workdir davidanson/markdownlint-cli2 --fix "/workdir/out/client/${filename_no_accent}/docs/*.md" "#node_modules"
+    -v ${PWD}:/workdir davidanson/markdownlint-cli2 --fix "/workdir/out/server/${filename_no_accent}/docs/*.md" "#node_modules"
 done
 
     docker run \
